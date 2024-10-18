19d81ae86e293841c28a9e8123b831dac1307da0
Billel Rachedi <snoprwaystv@gmail.com>
2024-10-18 13:05:34 +0100

    Saving the darkModeTheme with localstorage and handle the change in useEffect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,26 @@
 import { Link, NavLink } from "react-router-dom";
 import Logo from '../assets/images/logo.png'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Header = () => {
-  const [darkMode, setDarkMode] = useState(true);
   const [hidden, setHidden] = useState(true); 
+  const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem("darkMode")) || false);
 
   const activeClass = "block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
   const inActiveClass = "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
 
+      const handleTheme = () => {
+        if (darkMode) {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      }
 
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
+      useEffect(() => {
+        localStorage.setItem("darkMode", JSON.stringify(darkMode));
+        handleTheme(darkMode);
+      }, [darkMode]);
 
 
 
