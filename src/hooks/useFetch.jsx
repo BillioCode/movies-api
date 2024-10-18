514555c7708b07b27b2561f840db3e34f7a39277
Billel Rachedi <snoprwaystv@gmail.com>
2024-10-18 21:30:26 +0100

    added search feature and fixed the footer and then added movieDetail file etc

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect} from "react"
 
 
-export const useFetch = (url) => {
+export const useFetch = (apiPath,queryTerm="") => {
     const [data, setData] = useState([]);
+
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=afd804ef50f1e6b1ad6f29209e9395e6&query=${queryTerm}`;
+
 
     const fetchData = async (url) => {
         const response = await fetch(url);
--- src/route/AllRoutes.jsx
+++ src/route/AllRoutes.jsx
@@ -1,15 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import { MoviesList, MovieDetail, PageNotFound} from "../pages";
+import { MoviesList, MovieDetail,  Search, PageNotFound} from "../pages";
 
 
 export const AllRoutes = () => {
   return (
-    <Routes>
-      <Route path="" element={<MoviesList apiPath="movie/now_playing" />} />
-      <Route path="movie/:id" element={<MovieDetail />} />
-      <Route path="/movies/popular" element={<MoviesList apiPath="movie/popular" />} />
-      <Route path="/movies/top" element={<MoviesList apiPath="movie/top_rated" />} />
-      <Route path="/movies/upcoming" element={<MoviesList apiPath="movie/upcoming" />} />
-    </Routes>
+    <div className="dark:bg-slate-800">
+      <Routes>
+        <Route path="" element={<MoviesList apiPath="movie/now_playing" />} />
+        <Route path="movie/:id" element={<MovieDetail />} />
+        <Route path="/movies/popular" element={<MoviesList apiPath="movie/popular" />} />
+        <Route path="/movies/top" element={<MoviesList apiPath="movie/top_rated" />} />
+        <Route path="/movies/upcoming" element={<MoviesList apiPath="movie/upcoming" />} />
+        <Route path="search" element={<Search apiPath="search/movie" />} />
+      </Routes>
+    </div>
   )
 }
